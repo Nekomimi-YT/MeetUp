dead4ee29df823ea6eec05af46b01ab372ad9146
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-13 18:48:20 +0200

    Add div with class uncollapsedDetails

--- src/Event.js
+++ src/Event.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { mockData } from './mock-data';
 
 class Event extends Component {
 
   render() {
-    const { events } = this.props;
-    return <div className="collapsedDetails">
+    const { event } = this.props;
+    return <div>
+      <div className="collapsedDetails">
+        <h3>{mockData[0].summary}</h3>
+        <p>{mockData[0].start.dateTime}{mockData[0].start.timeZone}</p>
+        <p>{mockData[0].location}</p>
+      </div>
       <button className="DetailsButton">Show Details</button>
+      <div className="uncollapsedDetails">
+      </div>
     </div>
+    
   }
 }
 export default Event;
