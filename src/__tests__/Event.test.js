fc9932051d6f4fcb3b1bf03c03f4f0bceede098d
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-13 18:36:45 +0200

    Add test to confirm render of collapsed display information

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -4,13 +4,23 @@ import Event from '../Event';
 import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
+  
+  test('render collapsed event info container', () => {
+    const EventWrapper = shallow(<Event />);
+    expect(EventWrapper.find('.collapsedDetails')).toHaveLength(1);
+  });
+
+  test('render collapsed event info text', () => {
+    const event = mockData[0];
+    const EventWrapper = shallow(<Event event={event}/>);
+    expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.summary);
+    expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.start.dateTime);
+    expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.start.timeZone);
+    expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.location);
+  });
+
   test('render show details button', () => {
     const EventWrapper = shallow(<Event />);
     expect(EventWrapper.find('.DetailsButton')).toHaveLength(1);
   });
-
-  test('render collapsed details container', () => {
-    const EventWrapper = shallow(<Event />);
-    expect(EventWrapper.find('.collapsedDetails')).toHaveLength(1);
-  })
-});
+})
