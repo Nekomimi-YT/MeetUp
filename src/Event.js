ac8aa5d8e7d1e5fffd123fc9bfbaa9d5fb90162c
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-13 17:43:58 +0200

    Add props to render method and className collapsedDetails to div

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
   render() {
-    return <div>
+    const { events } = this.props;
+    return <div className="collapsedDetails">
       <button className="DetailsButton">Show Details</button>
     </div>
   }
