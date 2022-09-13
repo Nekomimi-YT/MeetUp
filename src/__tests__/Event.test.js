245fc144ca6d29adc07441b52042acf6a9b000df
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-13 17:46:09 +0200

    import mockData and test for collapsed details container

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Event from '../Event';
+import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
   test('render show details button', () => {
     const EventWrapper = shallow(<Event />);
     expect(EventWrapper.find('.DetailsButton')).toHaveLength(1);
   });
+
+  test('render collapsed details container', () => {
+    const EventWrapper = shallow(<Event />);
+    expect(EventWrapper.find('.collapsedDetails')).toHaveLength(1);
+  })
 });
