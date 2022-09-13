251efc4db8a808c6fb7bf74791c5a2660905b4af
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-13 14:31:24 +0200

    Add Show Details button

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    return <div>
+      <button className="DetailsButton">Show Details</button>
+    </div>
   }
 }
 export default Event;
