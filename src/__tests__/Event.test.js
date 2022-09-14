663d9d49067c96eb54340dedfafbd3810bca2785
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-14 14:55:04 +0200

    Update test for handleCollapsed method

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -12,7 +12,7 @@ describe('<Event /> component', () => {
 
   test('render collapsed event info text', () => {
     const event = mockData[0];
-    const EventWrapper = shallow(<Event event={event}/>);
+    const EventWrapper = shallow(<Event event={event}/>); // can I somehow send EventList's event prop?
     expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.summary);
     expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.start.dateTime);
     expect((EventWrapper.find('.collapsedDetails')).text()).toContain(event.start.timeZone);
@@ -36,18 +36,22 @@ describe('<Event /> component', () => {
     expect((EventWrapper.find('.uncollapsedDetails')).text()).toContain(event.description);
   });
 
-  test('change uncollapsed and buttonText state when button is clicked', () => {
+  test('change state with handleCollapse method when button is clicked', () => {
     const EventWrapper = shallow(<Event />);
-    EventWrapper.setState({ uncollapsed: false });
-    const { uncollapsed } = EventWrapper.state;
     const handleCollapse = EventWrapper.handleCollapse;
+    expect(EventWrapper.state('uncollapsed')).toBe(false); 
+    expect(EventWrapper.state('buttonText')).toBe('Show details');
     EventWrapper.find('.DetailsButton').simulate('click', handleCollapse);
-    if (uncollapsed === false) {
-      expect(EventWrapper.state('uncollapsed')).toBe(true);
-      expect(EventWrapper.state('buttonText')).toBe('Hide details');
-      } else {
-      expect(EventWrapper.state('uncollapsed')).toBe(false); 
-      expect(EventWrapper.state('buttonText')).toBe('Show details');
-    }
-  });  
+    expect(EventWrapper.state('uncollapsed')).toBe(true);
+    expect(EventWrapper.state('buttonText')).toBe('Hide details');
+    EventWrapper.find('.DetailsButton').simulate('click', handleCollapse);
+    expect(EventWrapper.state('uncollapsed')).toBe(false); 
+    expect(EventWrapper.state('buttonText')).toBe('Show details');
+  });
+
+  test('render state-specific innerText in button', () => {
+    const EventWrapper = shallow(<Event />);
+    const { buttonText } = EventWrapper.state; //returning an object => the whole state??
+    expect((EventWrapper.find('.DetailsButton')).text()).toContain({buttonText});
+  });
 })
