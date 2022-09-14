6b21f1fda3fff7869f2f20d264ed03e93ce012ac
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-14 17:11:21 +0200

    Update tests for uncollapsed container and text info when uncollapsed = true from button click

--- src/Event.js
+++ src/Event.js
@@ -22,19 +22,22 @@ class Event extends Component {
 
   render() {
     const { event } = this.props; //unable to use event in place of mockData[0]
-    const { buttonText } = this.state;
+    const { buttonText, uncollapsed } = this.state;
     return <div>
       <div className="collapsedDetails">
         <h3>{mockData[0].summary}</h3>
         <p>{mockData[0].start.dateTime}{mockData[0].start.timeZone}</p>
         <p>{mockData[0].location}</p>
       </div>
-      <div className="uncollapsedDetails">
-        <h4>About this event</h4>
-        {/*how to make this link to the text? */}
-        {mockData[0].htmlLink}See details on Google Calendar
-        <p>{mockData[0].description}</p>
-      </div>
+      {uncollapsed ? ( 
+        <div className="uncollapsedDetails">
+          <h4>About this event</h4>
+          {/*how to make this link to the text? */}
+          {mockData[0].htmlLink}See details on Google Calendar
+          <p>{mockData[0].description}</p>
+        </div>
+        ) : null
+      }
       <button className="DetailsButton" onClick={() => {this.handleCollapse()}}>{buttonText}</button>
     </div>
   }
