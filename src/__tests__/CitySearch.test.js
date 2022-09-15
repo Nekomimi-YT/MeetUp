d440e98136cdf8475892ff84fc01ef710ff7ea20
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-15 12:14:32 +0200

    Add beforeAll() method and remove individual assignment of CitySearchWrapper in each test

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -5,24 +5,25 @@ import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
+  let CitySearchWrapper;
+  beforeAll( () => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
+  
   test('render text input', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
 
   test('renders a list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 
   test('renders text input correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
   test('change state when text input changes', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper.setState({ query: 'Munich' });
     const eventObject = { target: { value: 'Berlin' }};
     CitySearchWrapper.find('.city').simulate('change', eventObject);
@@ -31,7 +32,6 @@ describe('<CitySearch /> component', () => {
 
   test('render list of suggestions correctly', () => {
     const locations = extractLocations(mockData);
-    const CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper.setState({ suggestions: locations });
     const suggestions = CitySearchWrapper.state('suggestions');
     expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
@@ -39,5 +39,4 @@ describe('<CitySearch /> component', () => {
       expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
     }
   });
-
 });
