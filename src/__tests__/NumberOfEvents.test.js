0ef7e187bc4066abce64976de4d30f2914e0b15b
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-15 12:19:37 +0200

    Add beforeAll() method and remove individual assignment of NumberOfEventsWrapper in each test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,29 +3,29 @@ import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents'
 
 describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll( () => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
   test('render text input', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.events')).toHaveLength(1);
   });
 
   test('text input id to be "eventNumber"', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.events').at(0).props().id).toEqual('eventNumber');
   });
 
   test('correct label for text input', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.eventLabel').text()).toBe('Number of Events');
   });
 
   test('renders text input correctly', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const eventItems = NumberOfEventsWrapper.state('eventItems');
     expect(NumberOfEventsWrapper.find('.events').prop('value')).toBe(eventItems);
   });
 
   test('change state when text input changes', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const eventObject = { target: { value: '10' }};
     NumberOfEventsWrapper.find('.events').simulate('change', eventObject);
     expect(NumberOfEventsWrapper.state('eventItems')).toBe('10');
