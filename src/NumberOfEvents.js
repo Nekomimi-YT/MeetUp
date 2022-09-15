67968b3bf69fe4c088678e63620d699fc442abfc
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-15 10:49:11 +0200

    Add label for text input, and input id and placeholder

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,9 +5,12 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents">
+      <label className="eventLabel" for="eventNumber">Number of Events</label>
       <input
       type="text"
       className="events"
+      id="eventNumber"
+      placeholder="32"
       />
     </div>
     );
