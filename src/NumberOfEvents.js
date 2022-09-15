acf38160a51d828cddff80b2eb7f3a4b5043cc82
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-15 10:07:27 +0200

    Add text input className = events to render method

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class NumberOfEvents extends Component {
 
   render() {
-    
+    return (
+      <div className="NumberOfEvents">
+      <input
+      type="text"
+      className="events"
+      />
+    </div>
+    );
   }
-
 }
 
 export default NumberOfEvents;
