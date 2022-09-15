f2b273a7aa8adf877f112c154a278c49de30a661
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-15 12:05:24 +0200

    Add onChange property to text input and call handleInputChanged method in component.  Add handleInputChanged method.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,17 +5,23 @@ class NumberOfEvents extends Component {
     eventItems: 32
   }
 
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ eventItems: value });
+  }
+
   render() {
     return (
       <div className="NumberOfEvents">
-      <label className="eventLabel" for="eventNumber">Number of Events</label>
-      <input
-      type="text"
-      className="events"
-      id="eventNumber"
-      value={this.state.eventItems}
-      />
-    </div>
+        <label className="eventLabel" for="eventNumber">Number of Events</label>
+        <input
+          type="text"
+          className="events"
+          id="eventNumber"
+          value={this.state.eventItems}
+          onChange={this.handleInputChanged}
+        />
+      </div>
     );
   }
 }
