813a093ebff18d1d3ee146099a8b4c98eda49723
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-16 13:40:52 +0200

    Add spaces between text variable elements

--- src/Event.js
+++ src/Event.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { mockData } from './mock-data';
 
 class Event extends Component {
   constructor(props) {
@@ -25,16 +24,15 @@ class Event extends Component {
     const { buttonText, uncollapsed } = this.state;
     return <div>
       <div className="collapsedDetails">
-        <h3>{mockData[0].summary}</h3>
-        <p>{mockData[0].start.dateTime}{mockData[0].start.timeZone}</p>
-        <p>{mockData[0].location}</p>
+        <h3>{event.summary}</h3>
+        <p>{event.start.dateTime} {event.start.timeZone}</p>
+        <p>{event.location}</p>
       </div>
       {uncollapsed ? ( 
         <div className="uncollapsedDetails">
           <h4>About this event</h4>
-          {/*how to make this link to the text? */}
-          {mockData[0].htmlLink}See details on Google Calendar
-          <p>{mockData[0].description}</p>
+          {event.htmlLink} See details on Google Calendar
+          <p>{event.description}</p>
         </div>
         ) : null
       }
