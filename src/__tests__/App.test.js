303834bc4fcc205e0910a86f7a46e053bbe889be
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-18 15:11:47 +0200

    Import mount from enzyme and code skeleton for first integration test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
@@ -22,4 +22,8 @@ describe('<App /> component', () => {
   test('render NumberOfEvents component', () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   })
+});
+
+describe('<App /> integration', () => {
+
 });
