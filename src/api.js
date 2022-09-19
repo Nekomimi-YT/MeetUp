e33ef34eecbf7d20ea26ac20b03836ca18c2be05
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-19 15:50:11 +0200

    Add checkToken method to chack for valid accessToken

--- src/api.js
+++ src/api.js
@@ -12,6 +12,16 @@ export const extractLocations = (events) => {
  return locations;
 };
 
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
 export const getEvents = async () => {
   return mockData;
 };
