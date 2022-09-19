cdd80f3e9e34aa51ec70cc44313c3a2292284d16
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-19 15:45:35 +0200

    Add function getAccessToken for export

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@
 * The Set will remove all duplicates from the array.
 */
 
+import axios from 'axios';
 import { mockData } from './mock-data';
 
 export const extractLocations = (events) => {
@@ -14,3 +15,23 @@ export const extractLocations = (events) => {
 export const getEvents = async () => {
   return mockData;
 };
+
+export const getAccessToken = async () => {
+  const getAccessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://nzmmemps58.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
