47226e01c17dad25b55b55d7e8f1ec93c6c57522
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-21 18:41:43 +0200

    Added afterEach() method and removed unmounting of AppWrapper after each test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -28,22 +28,24 @@ describe('<App /> component', () => {
 
 describe('<App /> integration', () => {
   let AppWrapper;
-  beforeAll( () => {
+  beforeEach( () => {
     AppWrapper = mount(<App />);
+  });
+
+  afterEach( () => {
+    AppWrapper.unmount();
   });
 
   test('App passes "events" state as a prop to EventList', () => {
     const AppEventsState = AppWrapper.state('events');
     expect(AppEventsState).not.toEqual(undefined);
     expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
-    AppWrapper.unmount();
   });
 
   test('App passes "locations" state as a prop to CitySearch', () => {
     const AppLocationsState = AppWrapper.state('locations');
     expect(AppLocationsState).not.toEqual(undefined);
     expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
-    AppWrapper.unmount();
   });
 
   test('get list of events matching the city selected by the user', async () => {
@@ -57,7 +59,6 @@ describe('<App /> integration', () => {
     const allEvents = await getEvents();
     const eventsToShow = allEvents.filter(event => event.location === selectedCity);
     expect(AppWrapper.state('events')).toEqual(eventsToShow);
-    AppWrapper.unmount();
   });
 
   test('get list of all events when user selects "See all cities"', async () => {
@@ -65,14 +66,12 @@ describe('<App /> integration', () => {
     await suggestionItems.at(suggestionItems.length - 1).simulate('click');
     const allEvents = await getEvents();
     expect(AppWrapper.state('events')).toEqual(allEvents);
-    AppWrapper.unmount();
   });
 
   test('App passes "numberOfEvents" state as a prop to NumberOfEvents', () => {
     const AppnumberOfEventsState = AppWrapper.state('numberOfEvents');
     expect(AppnumberOfEventsState).not.toEqual(undefined);
     expect(AppWrapper.find(NumberOfEvents).props().numberOfEvents).toEqual(AppnumberOfEventsState);
-    AppWrapper.unmount();
   });
 
   test('change number of events displayed after user changes number of events', () => {
@@ -84,19 +83,16 @@ describe('<App /> integration', () => {
     });
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(null, 1);
-    AppWrapper.unmount();
   });
 
   test('render list of events using mockData', () => {
     AppWrapper.setState({ events: mockData });
     expect(AppWrapper.find('.events')).toHaveLength(mockData.length);
-    AppWrapper.unmount();
   });
 
   test('change state after get list of events', async () => {
     AppWrapper.instance().updateEvents(null, 1);
     await AppWrapper.update();
     expect(await AppWrapper.state('events')).toHaveLength(1); // is this the right test?
-    AppWrapper.unmount();
   });
 });
