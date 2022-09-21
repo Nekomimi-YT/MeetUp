6a6fcbf4069bb7c3f1cdf8ac0eef410eca5cd7a4
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-21 17:12:42 +0200

    Add numberOfEvents to state, Destructure state variables in render method, Add comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       events: [],
-      locations: []
+      locations: [],
+      numberOfEvents: 32
     }
   }
 
@@ -19,7 +20,7 @@ class App extends Component {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({ events, locations: extractLocations(events) });// events: numberofEvents(events)
       }
     });
   }
@@ -28,7 +29,7 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
+  updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
@@ -39,12 +40,32 @@ class App extends Component {
     });
   }
 
+  /*return state (eventCount from NumberOfEvents, slice the first numOfEvents, assign to event state)
+  setNumberOfEvents = (numOfEvents) => {
+    getEvents().then((events) => {
+      if (numOfEvents <= events.length) {
+        this.setState ({
+          events: events.slice(0, numOfEvents)
+        });
+      } else if (numOfEvents == null) { //when the input is blank ??
+        this.setState ({
+          events: events.slice(0, 0)
+        });
+      } else {
+        this.setState ({
+          events: events.slice(0, 32) //max number of events shown is fixed at 32
+        });
+      }
+    });
+  }*/
+
   render() {
+    const { events, locations, numberOfEvents } = this.state;
     return (
       <div className="App">
-        <CitySearch locations={ this.state.locations } updateEvents={ this.updateEvents }/>
-        <NumberOfEvents />
-        <EventList events={ this.state.events } />
+        <CitySearch locations={ locations } updateEvents={ this.updateEvents }/>
+        <NumberOfEvents updateEvents={ this.updateEvents } numberOfEvents={ numberOfEvents }/>  {/*Pass function as prop to return eventCount*/}
+        <EventList events={ events } />
       </div>
     );
   }
