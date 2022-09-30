a45dbb7450b2d51a00e01616a7918bcb3a4dd883
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-30 14:03:54 +0200

    define AppWrapper, mount and import React, mount, App and mockData

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,4 +1,8 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { mount } from 'enzyme';
+import App from '../App';
+import { mockData } from '../mock-data';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -9,8 +13,9 @@ defineFeature(feature, test => {
 
     });
 
-    
+    let AppWrapper;
     when('the user opens the app', () => {
+      AppWrapper = mount(<App />);
 
     });
 
