4fd0901a84edf6dbf98beb65e6f45569be8f87f6
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-01 10:27:24 +0200

    Add to puppeteer method to view tests in browser - and then comment out

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,8 +4,13 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
-    jest.setTimeout(30000);
+    jest.setTimeout(50000);
     browser = await puppeteer.launch();
+    /*browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+     ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+    });*/
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
