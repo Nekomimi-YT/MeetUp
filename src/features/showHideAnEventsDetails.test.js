02535d3a4b4a73f4620231c08f3031fc4b24526d
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-01 14:06:37 +0200

    Update first scenario test and add second.  Import Event.js

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
+import Event from '../Event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
   test('An event element is collapsed by default', ({ given, when, then }) => {
-    let AppWrapper;
+    let EventWrapper;
+    let event = mockData[0];
     given('user is using the app within its default setting', () => {
-      AppWrapper = mount(<App />);
+      EventWrapper = mount(<Event event={event}/>);
     });
 
     when('the user observes the event element', () => {
@@ -18,23 +20,28 @@ defineFeature(feature, test => {
     });
 
     then('the element is collapsed (details not showing)', () => {
-      AppWrapper.update();
-      expect(AppWrapper.find('.collapsedDetails')).toHaveLength(2);
-      expect(AppWrapper.find('.uncollapsedDetails')).toHaveLength(0);
+      EventWrapper.update();
+      expect(EventWrapper.find('.collapsedDetails')).toHaveLength(1);
+      expect(EventWrapper.find('.uncollapsedDetails')).toHaveLength(0);
     });
   });
 
   test('User can expand an event to see its details', ({ given, when, then }) => {
+    let EventWrapper;
+    let event = mockData[0];
     given('a user wants to see more details about an event', () => {
-
+      EventWrapper = mount(<Event event={event}/>);
     });
 
     when('the user interacts with the event element', () => {
-
+    EventWrapper.update();
+    const handleCollapse = EventWrapper.handleCollapse;
+    EventWrapper.find('.details-btn').simulate('click', handleCollapse);
     });
 
     then('it should expand to show details', () => {
-
+      EventWrapper.update();
+      expect(EventWrapper.find('.uncollapsedDetails')).toHaveLength(1);
     });
   });
 
