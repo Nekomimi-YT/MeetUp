77141edd3260bd9616edaebebd2779c06ac396b5
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-01 13:29:52 +0200

    Add remaining skeleton tests

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -3,6 +3,33 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
+  test('An event element is collapsed by default', ({ given, when, then }) => {
+    given('user is using the app within its default setting', () => {
+
+    });
+
+    when('the user observes the event element', () => {
+
+    });
+
+    then('the element is collapsed (details not showing)', () => {
+
+    });
+  });
+
+  test('User can expand an event to see its details', ({ given, when, then }) => {
+    given('a user wants to see more details about an event', () => {
+
+    });
+
+    when('the user interacts with the event element', () => {
+
+    });
+
+    then('it should expand to show details', () => {
+
+    });
+  });
 
   test('User can collapse an event to hide its details', ({ given, when, then }) => {
     given('a user wants to minimize an event details element', () => {
