39a66b89bacec9bce2b3ef330da8322f77a2b472
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-01 15:15:12 +0200

    Add details to first scenario test and import App

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,30 +1,34 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { mockData } from '../mock-data';
-import Event from '../Event';
+import NumberOfEvents from '../NumberOfEvents'
+import App from '../App'
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
   test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
+    let AppWrapper;
     given('a user has not specified a preferred number of events', () => {
-
+      AppWrapper = shallow(<App />);
     });
 
     when('the app displays events', () => {
-
+      AppWrapper.update();
     });
 
     then('the default number (32) is displayed', () => {
-
+      const AppNumOfEventsState = AppWrapper.state('numberOfEvents');
+      expect(AppNumOfEventsState).toEqual(32);
     });
   });
 
   test('User can change the number of events they want to see', ({ given, when, then }) => {
+    let AppWrapper, NumberOfEventsWrapper;
     given('the user wants to change the number of events displayed', () => {
-
+      AppWrapper = mount(<App />);
     });
 
     when('the user types the preferred number into the text box', () => {
