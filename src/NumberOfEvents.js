61eac0836061eaefa6762e7e41c19d205faf6faa
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-04 19:32:43 +0200

    update code to reduce repetition

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -9,16 +9,16 @@ class NumberOfEvents extends Component {
 
   handleInputChanged = (event) => {
     let value = event.target.value;
+    this.setState ({ numberOfEvents: value });
     //let r = /[1-9]|1[0-9]|2\d|3[012]/; Can we use this regex instead?
     if (value < 1 || value > 32) {
       this.setState ({
-        errorText: 'Please enter a number between 1-32.',
-        numberOfEvents: value
+        errorText: 'Please enter a number between 1-32.'
       });
     } else {
     this.setState({ 
-      errorText: '',
-      numberOfEvents: value });
+      errorText: ''
+    });
     }
     this.props.updateEvents(null, value);
   }
