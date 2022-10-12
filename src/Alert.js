c0f7b84efe835ceb86f72248907080d1dbe9bc79
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-12 11:41:09 +0200

    Add fontSize to class constructor

--- src/Alert.js
+++ src/Alert.js
@@ -8,7 +8,8 @@ class Alert extends Component {
 
   getStyle = () => {
     return {
-      color: this.color
+      color: this.color,
+      fontSize: this.fontSize
     };
   }
 
@@ -25,6 +26,7 @@ class InfoAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = 'blue';
+    this.fontSize = '14px';
   }
 }
 
@@ -32,6 +34,7 @@ class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = 'red';
+    this.fontSize = '14px';
   }
 }
 
@@ -39,6 +42,7 @@ class OfflineAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = 'black';
+    this.fontSize = '18px';
   }
 }
 
