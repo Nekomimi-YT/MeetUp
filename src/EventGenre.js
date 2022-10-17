0efb953c6b116f94e4869e976c194b97a5d401d3
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-17 17:56:08 +0200

    Add getData method to EventGenre component

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 }
-  ];
+  
+  getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const data = genres.map((genre) => {
+      const value = events.filter(({ summary }) =>
+          summary.split(' ').includes(genre)
+        ).length;
+        return { name: genre, value };
+    });
+    return data;
+  };
+
   return (
     <ResponsiveContainer height={400} >
-      <PieChart width={400} height={400}>
+      <PieChart width={400} height={400} >
         <Pie
           data={data}
           cx={200}
