024981453994962258c7ac43417b38d146f613d9
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-18 16:59:09 +0200

    Update styling

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ class App extends Component {
           <h4>Events in each city</h4>
           <div className="data-vis-wrapper">
             <EventGenre events={events} />
-            <ResponsiveContainer height={400}>
+            <ResponsiveContainer className="recharts-responsive-container" height={400}>
               <ScatterChart
                 margin={{
                   top: 20, right: 20, bottom: 20, left: 20,
